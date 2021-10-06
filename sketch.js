e1e403fe9e3cd60a09b33972ac105ff81d549db2
TopprCodr <72185815+TopprCodr@users.noreply.github.com>
2021-10-06 18:01:04 +0530

    boilerplate

---
+++ sketch.js
@@ -0,0 +1,53 @@
+var Engine = Matter.Engine,
+Render = Matter.Render,
+Runner = Matter.Runner,
+Body = Matter.Body,
+Events = Matter.Events,
+Composite = Matter.Composite,
+Composites = Matter.Composites,
+Common = Matter.Common,
+MouseConstraint = Matter.MouseConstraint,
+Mouse = Matter.Mouse,
+Bodies = Matter.Bodies;
+
+// create engine
+var engine = Engine.create(),
+world = engine.world;
+
+// create renderer
+var render = Render.create({
+element: document.body,
+engine: engine,
+options: {
+    width: 1500,
+    height: 600,
+    wireframes: false,
+    background: "#B5EAEA",
+}
+});
+
+Render.run(render);
+
+// create runner
+var runner = Runner.create();
+Runner.run(runner, engine);
+
+
+
+
+
+var mouse = Mouse.create(render.canvas),
+mouseConstraint = MouseConstraint.create(engine, {
+    mouse: mouse,
+    constraint: {
+        stiffness: 0.2,
+        render: {
+            visible: false
+        }
+    }
+});
+
+Composite.add(world, mouseConstraint);
+
+// keep the mouse in sync with rendering
+render.mouse = mouse;
