7f4553ac0f7d30e9cd4b77b7f3e56a31f25a0289
[gitika] <[gitikakhira69]>
2021-11-24 21:03:17 +0530

    85

--- sketch.js
+++ sketch.js
@@ -34,35 +34,52 @@ Runner.run(runner, engine);
 
 var wallStyle = {fillStyle:"black"};
 
-var wall1 = Matter.Bodies.rectangle(400, 0, 800, 50, { isStatic: true, render: {fillStyle:"#a242"} }),
+var wall1 = Matter.Bodies.rectangle(400, 0, 800, 50, { isStatic: true, render:{wallStyle}}),
     wall2 = Bodies.rectangle(400,600,800,50,{isStatic:true,render:{wallStyle}}),
     wall3 = Bodies.rectangle(800,0,50,230,{isStatic:true,render:{wallStyle}}),
     wall4 = Bodies.rectangle(800,450,50,350,{isStatic:true,render:{wallStyle}}),
     wall5 = Bodies.rectangle(0,300,50,600,{isStatic:true,render:{wallStyle}});
 
-// var smile = Composites.stack(300,300,3,5,2,2,(x,y)=>{
-//     return Bodies.circle(x,y,50);
-//     render:{
-//         sprite:{
-//             texture:"images/Smile.png"
-//         }
-//     }
-// })
+var bodystyle = {sprite:{texture:"images/Smile.png",xScale:0.2,yScale:0.2}}
+var pyramid = Composites.pyramid(0,0,11,6,50,50,(x,y)=>{
+    return Bodies.circle(x,y,15,{restituion:1,render:bodystyle,frictionAir:0.1})
+    
+});
 
- Composite.add(world,{wall1,wall2,wall3,wall4,wall5,smile}) 
 
-// var mouse = Mouse.create(render.canvas),
-// mouseConstraint = MouseConstraint.create(engine, {
-//     mouse: mouse,
-//     constraint: {
-//         stiffness: 0.2,
-//         render: {
-//             visible: false
-//         }
-//     }
-// });
+Events.on(engine, "collisionStart",function (event){
+    var pairs = event.pairs;
+    console.log(event)
+    for(var i=0;i<pairs.length;i++){
+        var pair = pairs[i]
+        pair.bodyA.render.fillStyle="red";
+        pair.bodyB.render.sprite.texture="images/Goofball.png"
+    }
+})
+Events.on(engine, "collisionActive",function (event){
+    var pairs = event.pairs;
+    
+    for(var i=0;i<pairs.length;i++){
+        var pair = pairs[i]
+        pair.bodyA.render.fillStyle="black";
+        pair.bodyB.render.sprite.texture="images/Smile.png"
+    }
+})
 
-// Composite.add(world, mouseConstraint);
+ Composite.add(world,[wall1,wall2,wall3,wall4,wall5,pyramid]) 
+
+var mouse = Mouse.create(render.canvas),
+mouseConstraint = MouseConstraint.create(engine, {
+    mouse: mouse,
+    constraint: {
+        stiffness: 0.2,
+        render: {
+            visible: false
+        }
+    }
+});
+
+Composite.add(world, mouseConstraint);
 
 // keep the mouse in sync with rendering
-// render.mouse = mouse;
+render.mouse = mouse;
